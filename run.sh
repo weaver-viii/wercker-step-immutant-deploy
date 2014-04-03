881eb0f759a2242da88662f6911a2a1efd2bd137
Chris Shea <cmshea@gmail.com>
2014-04-03 17:36:26 -0400

    [CS] Make bash execute the script

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ scp -i $PRIVATEKEY_PATH -o "StrictHostKeyChecking no" -P 2222 $WERCKER_STEP_ROOT
 scp -i $PRIVATEKEY_PATH -o "StrictHostKeyChecking no" -P 2223 $WERCKER_STEP_ROOT/$IMMUTANT_SCRIPT deploy@localhost:$IMMUTANT_SCRIPT
 
 # Run the script on the servers
-ssh -i $PRIVATEKEY_PATH -o "StrictHostKeyChecking no" -p 2222 deploy@localhost "$IMMUTANT_SCRIPT"
-ssh -i $PRIVATEKEY_PATH -o "StrictHostKeyChecking no" -p 2223 deploy@localhost "$IMMUTANT_SCRIPT"
+ssh -i $PRIVATEKEY_PATH -o "StrictHostKeyChecking no" -p 2222 deploy@localhost "bash $IMMUTANT_SCRIPT"
+ssh -i $PRIVATEKEY_PATH -o "StrictHostKeyChecking no" -p 2223 deploy@localhost "bash $IMMUTANT_SCRIPT"
